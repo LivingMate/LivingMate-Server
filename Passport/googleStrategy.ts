422a4855cd35e574889467e80409b98be6f673f3
Junyu Park <everafter1112@gmail.com>
2023-12-05 16:55:43 +0900

    authentication with google

---
+++ Passport/googleStrategy.ts
@@ -0,0 +1,48 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import {PrismaClient} from '@prisma/client';
+import { UserService } from '../Services';
+
+const prisma = new PrismaClient;
+
+//const User = require('../models/user');
+
+module.exports = () => {
+   passport.use(
+      new GoogleStrategy(
+         {
+            clientID: process.env.GOOGLE_ID, // 구글 로그인에서 발급받은 REST API 키
+            clientSecret: process.env.GOOGLE_SECRET,
+            callbackURL: '/auth/google/callback', // 구글 로그인 Redirect URI 경로
+         },
+         async (accessToken, refreshToken, profile, done) => {
+            console.log('google profile : ', profile);
+            try {
+               const exUser = await prisma.user.findUnique({
+                  // 구글 플랫폼에서 로그인 했고 & snsId필드에 구글 아이디가 일치할경우
+                  where: { email: profile.email},
+               });
+               // 이미 가입된 구글 프로필이면 성공
+               if (exUser) {
+                  done(null, exUser); // 로그인 인증 완료
+               } else {
+                  // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
+                  const newUser = await prisma.user.create({
+                    data:{
+                        email: profile?.email[0].value,
+                        userName: profile.displayName,
+                        userColor: 'FFFFFF', 
+                        sex: profile.sex,
+                        age: profile.age,
+                    }
+                  });
+                  done(null, newUser); // 회원가입하고 로그인 인증 완료
+               }
+            } catch (error) {
+               console.error(error);
+               done(error);
+            }
+         },
+      ),
+   );
+};
--- Routers/AuthRouter.ts
+++ Routers/AuthRouter.ts
@@ -1,6 +1,17 @@
 import { Router } from 'express';
-import { body } from 'express-validator/check';
+import { body } from 'express-validator';
 import { AuthController } from '../Controllers';
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 const router:Router = Router();
 
+router.get('/google', passport.authenticate('google', {scope:['profile', 'email']}));
+
+router.get('/google/callback',passport.authenticate('google', {failureRedirect: '/'}),
+            (req,res) =>{
+                res.redirect('/');
+            },
+);
