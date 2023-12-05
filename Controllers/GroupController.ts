d040cca6808f41a303313eb111c3fb88254907f1
깐돌 <eonwy@kkandol-ui-MacBookAir.local>
2023-12-05 15:03:36 +0900

    1205 eonwy-2

--- Controllers/GroupController.ts
+++ Controllers/GroupController.ts
@@ -4,3 +4,15 @@ import { GroupResponseDto } from "../DTOs/Group/Responses/GroupResponseDto";
 import { GroupJoinResponseDto } from "../DTOs/Group/Responses/GroupJoinResponseDto";
 
 
+const getGroup =async (req:Request, res:Response, next:NextFunction) => {
+    
+}
+
+// const createGroup
+
+// const joinGroup
+
+// const leaveGroup
+
+
+
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 import router from './Routers/index'; 
-app.use('/feed', router); 
+
+// app.use('/feed', router); 
+app.use('/calendar', router);
 
 app.listen(3000, () => {
   console.log('서버가 3000번 포트에서 실행 중');
