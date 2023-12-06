e7109c42887740435ab4993b03603926c8311b50
깐돌 <eonwy@kkandol-ui-MacBookAir.local>
2023-12-06 15:35:50 +0900

    1206

--- dist/Services/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//우리는 이게 없어서... 이걸 만들어서 export를 하면 다른 데에서 한번에 import할 수 있을지 몰라서 일단 한번 해 봤어. 
-//UserService folder만든것도 마찬가지 이유.. 
-//# sourceMappingURL=index.js.map
