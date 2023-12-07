78ebb46ac949c3ed84fb6935c4b288ad0c4da9b8
Junyu Park <everafter1112@gmail.com>
2023-12-07 16:51:42 +0900

    help

--- src/Passport/index.ts
+++ src/Passport/index.ts
@@ -1,26 +1,25 @@
-import passport from 'passport';
-import {PrismaClient} from '@prisma/client';
-import { UserService } from '../Services';
-import google from './googleStrategy';
+// import passport from 'passport';
+// import {PrismaClient} from '@prisma/client';
+// import google from './googleStrategy';
 
 
-const prisma = new PrismaClient;
+// const prisma = new PrismaClient;
 
-module.exports = () => {
+// module.exports = () => {
 
-    passport.serializeUser((user, done) => {
-       done(null, user.id);
-    });
+//     passport.serializeUser((user, done) => {
+//        done(null, user.id);
+//     });
  
-    passport.deserializeUser((id, done) => {
-     prisma.user.findUnique({
-         where:{
-             id:id,
-         }
-     }).then (user=>done(null,user))
-     .catch(err=>done(err))
-    }); 
+//     passport.deserializeUser((id, done) => {
+//      prisma.user.findUnique({
+//          where:{
+//              id:id,
+//          }
+//      }).then (user=>done(null,user))
+//      .catch(err=>done(err))
+//     }); 
  
     
-    google(); // 구글 전략 등록
- };
+//     google(); // 구글 전략 등록
+//  };
