be6ef50e02314aa76786047b6a2339d849f462fa
Junyu Park <everafter1112@gmail.com>
2023-12-16 20:06:22 +0900

    run

--- dist/DTOs/Calendar/Request/CalendarUpdateDto.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=CalendarUpdateDto.js.map
--- dist/Routers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { Router } from 'express'
-// import FeedRouter from './FeedRouter'
-// import CalendarRouter from './CalendarRouter'
-// const router: Router = Router()
-// router.use('/feed', FeedRouter)
-// router.use('/Calendar', CalendarRouter)
-// export default router;
-//# sourceMappingURL=index.js.map
