95751426356187505a22308e25dc26e4c0e01acd
Junyu Park <everafter1112@gmail.com>
2023-12-18 21:03:14 +0900

    neatifying index.ts

--- src/index.ts
+++ src/index.ts
@@ -91,6 +91,4 @@ app.use(
 //! express-session에 의존하므로 뒤에 위치해야 함
 app.use(passport.initialize()); // 요청 객체에 passport 설정을 심음
 app.use(passport.session()); // req.session 객체에 passport정보를 추가 저장
-// passport.session()이 실행되면, 세션쿠키 정보를 바탕으로 해서 passport/index.js의 deserializeUser()가 실행하게 한다.
-
-
+// passport.session()이 실행되면, 세션쿠키 정보를 바탕으로 해서 passport/index.js의 deserializeUser()가 실행하게 한다.*/
