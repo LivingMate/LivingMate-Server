11d1309dd4a4b28cfdc4af4de6f93ed413ac02c2
Junyu Park <everafter1112@gmail.com>
2024-02-02 21:16:41 +0900

    login issue fixed!

--- src/index.ts
+++ src/index.ts
@@ -49,18 +49,6 @@ app.listen(3000, () => {
 
 
 
-// app.use(function (err: ErrorType, req: Request, res: Response, next: NextFunction) {
-//   res.locals.message = err.message
-//   res.locals.error = req.app.get('env') === 'production' ? err : {}
-
-//   // render the error page
-//   res.status(err.status || 500)
-//   res.render('error')
-// })
-
-
-//const passportConfig = require('./passport');
-//passportConfig(); // 패스포트 설정
 
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -75,7 +63,4 @@ app.use(
       },
    }),
 );
-//! express-session에 의존하므로 뒤에 위치해야 함
-app.use(passport.initialize()); // 요청 객체에 passport 설정을 심음
-app.use(passport.session()); // req.session 객체에 passport정보를 추가 저장
-// passport.session()이 실행되면, 세션쿠키 정보를 바탕으로 해서 passport/index.js의 deserializeUser()가 실행하게 한다.*/
+
